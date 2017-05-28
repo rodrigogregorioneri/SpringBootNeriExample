1fd20f3bcccd8989b402be59b70a79df79f09bd1
rodrigogregorioneri <rodrigogregorioneri@hotmail.com>
2017-05-28 20:55:14 -0300

    commit inicial

---
+++ springproject/src/main/java/SpringBootMain.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {  "controllers", "app"  })
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class SpringBootMain extends SpringBootServletInitializer {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMain.class, args);
+	}
+}
---
+++ springproject/src/main/java/controllers/User.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class User {
+
+	   @RequestMapping("/")
+	    @ResponseBody
+	    String home() {
+	        return "Hello World!";
+	    }
+	
+}
